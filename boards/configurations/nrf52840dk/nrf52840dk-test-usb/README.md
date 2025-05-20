5196fa62385e2bc7f1d8749dbf169ea797ff008a
charles37 <benprevor@gmail.com>
2025-05-20 13:33:10 -0400

    new usb capsule implementation

--- boards/configurations/nrf52840dk/nrf52840dk-test-usb/README.md
+++ boards/configurations/nrf52840dk/nrf52840dk-test-usb/README.md
@@ -1,6 +1,6 @@
 # nRF52840DK USB Configuration
 
-This board configuration is designed to enable USB functionality on the Nordic Semiconductor nRF52840 development kit.
+This board configuration is designed to enable USB functionality on the Nordic Semiconductor nRF52840 development kit with minimal dependencies.
 
 ## Features
 
@@ -8,6 +8,90 @@ This board configuration is designed to enable USB functionality on the Nordic S
 - Demonstrates USB driver implementation in Tock
 - Uses the nRF52840's built-in USB peripheral
 - Provides a virtual serial port for communication between the board and host computer
+- Exposes the USB functionality to userspace applications via the USB syscall driver
+
+## Implementation Details
+
+The key components of the USB implementation are:
+
+1. **USB CDC-ACM Driver Setup**
+   ```rust
+   // Create a virtual timer for USB CDC-ACM
+   let virtual_alarm_cdc = static_init!(
+       VirtualMuxAlarm<'static, nrf52::rtc::Rtc>,
+       VirtualMuxAlarm::new(mux_alarm)
+   );
+   virtual_alarm_cdc.setup();
+
+   // Define USB string descriptors
+   let strings = static_init!(
+       [&str; 3],
+       [
+           "Tock",                // Manufacturer
+           "nRF52840-DK USB CDC", // Product
+           "serial0001",          // Serial number
+       ]
+   );
+
+   // Setup the USB CDC-ACM driver
+   let usb_cdc = components::cdc::CdcAcmComponent::new(
+       &nrf52840_peripherals.usbd,
+       capsules_extra::usb::usbc_client::MAX_CTRL_PACKET_SIZE_NRF52840,
+       0x1915, // Nordic Vendor ID
+       0x1001, // Product ID (arbitrary)
+       strings,
+       mux_alarm,
+       Some(&usb_reset_bootloader_enter),
+   )
+   .finalize(components::cdc_acm_component_static!(
+       nrf52::usbd::Usbd,
+       nrf52::rtc::Rtc
+   ));
+
+   // Enable USB CDC-ACM driver
+   usb_cdc.enable();
+   usb_cdc.attach();
+   ```
+
+2. **USB Syscall Driver for Userspace Access**
+   ```rust
+   // Create USB syscall driver for userspace access
+   let usb_driver = static_init!(
+       UsbSyscallDriver<'static, capsules_extra::usb::cdc::CdcAcm<...>>,
+       UsbSyscallDriver::new(
+           usb_cdc, 
+           board_kernel.create_grant(
+               capsules_extra::usb::usb_user::DRIVER_NUM, 
+               &memory_allocation_capability
+           )
+       )
+   );
+   ```
+
+3. **Platform Integration**
+   ```rust
+   // Add to platform struct
+   pub struct Platform {
+       // ...
+       usb_cdc: &'static capsules_extra::usb::cdc::CdcAcm<...>,
+       usb_driver: &'static UsbSyscallDriver<'static, capsules_extra::usb::cdc::CdcAcm<...>>,
+       // ...
+   }
+
+   // Add to syscall driver lookup
+   impl SyscallDriverLookup for Platform {
+       fn with_driver<F, R>(&self, driver_num: usize, f: F) -> R
+       where
+           F: FnOnce(Option<&dyn kernel::syscall::SyscallDriver>) -> R,
+       {
+           match driver_num {
+               // ...
+               capsules_extra::usb::usb_user::DRIVER_NUM => f(Some(self.usb_driver)),
+               // ...
+           }
+       }
+   }
+   ```
 
 ## Usage
 
@@ -16,6 +100,15 @@ This configuration can be used for:
 1. Testing USB functionality in the Tock kernel
 2. Developing user applications that interact with USB devices
 3. Serial communication over USB instead of UART
+
+## USB Driver Details
+
+This configuration uses the built-in USB peripheral of the nRF52840 SoC and implements CDC-ACM (Communication Device Class - Abstract Control Model) to create a virtual serial port. When connected to a computer, it will appear as a standard serial device.
+
+The default USB descriptors identify the device as:
+- Manufacturer: "Tock"  
+- Product: "nRF52840-DK USB CDC"
+- Serial: "serial0001"
 
 ## Building and Flashing
 
@@ -33,11 +126,6 @@ cd boards/configurations/nrf52840dk/nrf52840dk-test-usb
 make flash
 ```
 
-## USB Driver Details
+## Using the USB Driver in Userspace Applications
 
-This configuration uses the built-in USB peripheral of the nRF52840 SoC and implements CDC-ACM (Communication Device Class - Abstract Control Model) to create a virtual serial port. When connected to a computer, it will appear as a standard serial device.
-
-The default USB descriptors identify the device as:
-- Manufacturer: "Tock"  
-- Product: "nRF52840-DK USB CDC"
-- Serial: "serial0001"
+Applications can use the USB driver by making syscalls to driver number `0x20` (USB driver), which is defined as `capsules_extra::usb::usb_user::DRIVER_NUM`. This allows userspace applications to send and receive data over the USB interface.
--- boards/configurations/nrf52840dk/nrf52840dk-test-usb/src/main.rs
+++ boards/configurations/nrf52840dk/nrf52840dk-test-usb/src/main.rs
@@ -3,7 +3,7 @@
 // Copyright Tock Contributors 2024.
 
 //! Tock kernel for the Nordic Semiconductor nRF52840 development kit (DK)
-//! with USB functionality enabled.
+//! with minimal USB functionality enabled.
 
 #![no_std]
 #![no_main]
@@ -14,33 +14,10 @@ use core::ptr::addr_of;
 use capsules_core::virtualizers::virtual_alarm::VirtualMuxAlarm;
 use capsules_extra::usb::usb_user::UsbSyscallDriver;
 use kernel::component::Component;
-use kernel::hil::led::LedLow;
-use kernel::hil::time::Counter;
-use kernel::hil::usb::Client;
 use kernel::platform::{KernelResources, SyscallDriverLookup};
 use kernel::scheduler::round_robin::RoundRobinSched;
 use kernel::{capabilities, create_capability, debug, static_init};
-use nrf52840::gpio::Pin;
 use nrf52840::interrupt_service::Nrf52840DefaultPeripherals;
-use nrf52_components::{UartChannel, UartPins};
-
-// The nRF52840DK LEDs (see back of board)
-const LED1_PIN: Pin = Pin::P0_13;
-const LED2_PIN: Pin = Pin::P0_14;
-const LED3_PIN: Pin = Pin::P0_15;
-const LED4_PIN: Pin = Pin::P0_16;
-
-// The nRF52840DK buttons (see back of board)
-const BUTTON1_PIN: Pin = Pin::P0_11;
-const BUTTON2_PIN: Pin = Pin::P0_12;
-const BUTTON3_PIN: Pin = Pin::P0_24;
-const BUTTON4_PIN: Pin = Pin::P0_25;
-const BUTTON_RST_PIN: Pin = Pin::P0_18;
-
-const UART_RTS: Option<Pin> = Some(Pin::P0_05);
-const UART_TXD: Pin = Pin::P0_06;
-const UART_CTS: Option<Pin> = Some(Pin::P0_07);
-const UART_RXD: Pin = Pin::P0_08;
 
 /// Debug Writer
 pub mod io;
@@ -82,27 +59,11 @@ fn usb_reset_bootloader_enter() {
 //------------------------------------------------------------------------------
 
 type AlarmDriver = components::alarm::AlarmDriverComponentType<nrf52840::rtc::Rtc<'static>>;
-type RngDriver = components::rng::RngComponentType<nrf52840::trng::Trng<'static>>;
 
 /// Supported drivers by the platform
 pub struct Platform {
-    ble_radio: &'static capsules_extra::ble_advertising_driver::BLE<
-        'static,
-        nrf52::ble_radio::Radio<'static>,
-        VirtualMuxAlarm<'static, nrf52::rtc::Rtc<'static>>,
-    >,
     console: &'static capsules_core::console::Console<'static>,
-    button: &'static capsules_core::button::Button<'static, nrf52840::gpio::GPIOPin<'static>>,
-    gpio: &'static capsules_core::gpio::GPIO<'static, nrf52840::gpio::GPIOPin<'static>>,
-    led: &'static capsules_core::led::LedDriver<
-        'static,
-        kernel::hil::led::LedLow<'static, nrf52840::gpio::GPIOPin<'static>>,
-        4,
-    >,
-    rng: &'static RngDriver,
-    alarm: &'static AlarmDriver,
-    // The USB CDC-ACM driver
-    // Used for panic output through the USB CDC-ACM interface
+    // The USB CDC-ACM driver - used for panic output through the USB CDC-ACM interface
     #[allow(dead_code)]
     usb_cdc: &'static capsules_extra::usb::cdc::CdcAcm<
         'static,
@@ -115,6 +76,7 @@ pub struct Platform {
         nrf52::usbd::Usbd<'static>,
         VirtualMuxAlarm<'static, nrf52::rtc::Rtc<'static>>
     >>,
+    alarm: &'static AlarmDriver,
     scheduler: &'static RoundRobinSched<'static>,
     systick: cortexm4::systick::SysTick,
     ipc: kernel::ipc::IPC<{ NUM_PROCS as u8 }>,
@@ -127,12 +89,7 @@ impl SyscallDriverLookup for Platform {
     {
         match driver_num {
             capsules_core::console::DRIVER_NUM => f(Some(self.console)),
-            capsules_core::gpio::DRIVER_NUM => f(Some(self.gpio)),
             capsules_core::alarm::DRIVER_NUM => f(Some(self.alarm)),
-            capsules_core::led::DRIVER_NUM => f(Some(self.led)),
-            capsules_core::button::DRIVER_NUM => f(Some(self.button)),
-            capsules_core::rng::DRIVER_NUM => f(Some(self.rng)),
-            capsules_extra::ble_advertising_driver::DRIVER_NUM => f(Some(self.ble_radio)),
             capsules_extra::usb::usb_user::DRIVER_NUM => f(Some(self.usb_driver)),
             kernel::ipc::DRIVER_NUM => f(Some(&self.ipc)),
             _ => f(None),
@@ -210,16 +167,6 @@ pub unsafe fn main() {
     // BOARD INITIALIZATION
     //--------------------------------------------------------------------------
 
-    // Choose regular UART for serial
-    let uart_channel = UartChannel::Pins(UartPins::new(UART_RTS, UART_TXD, UART_CTS, UART_RXD));
-
-    // Configure kernel debug GPIOs as early as possible
-    kernel::debug::assign_gpios(
-        Some(&nrf52840_peripherals.gpio_port[LED1_PIN]),
-        Some(&nrf52840_peripherals.gpio_port[LED2_PIN]),
-        Some(&nrf52840_peripherals.gpio_port[LED3_PIN]),
-    );
-
     let board_kernel = static_init!(kernel::Kernel, kernel::Kernel::new(&*addr_of!(PROCESSES)));
 
     // Create an nRF52 chip for memory protection and timers.
@@ -232,7 +179,7 @@ pub unsafe fn main() {
     // Do nRF52 specific startup
     nrf52_components::startup::NrfStartupComponent::new(
         false,
-        BUTTON_RST_PIN,
+        nrf52840::gpio::Pin::P0_18, // BUTTON_RST_PIN
         nrf52840::uicr::Regulator0Output::DEFAULT,
         &base_peripherals.nvmc,
     )
@@ -266,6 +213,20 @@ pub unsafe fn main() {
     // UART & CONSOLE & DEBUG
     //--------------------------------------------------------------------------
 
+    // Configure UART pins
+    let uart_channel = nrf52_components::UartChannel::Pins(nrf52_components::UartPins::new(
+        Some(nrf52840::gpio::Pin::P0_05), // RTS
+        nrf52840::gpio::Pin::P0_06,       // TXD
+        Some(nrf52840::gpio::Pin::P0_07), // CTS
+        nrf52840::gpio::Pin::P0_08,       // RXD
+    ));
+
+    // Process printer for debugging
+    let process_printer = components::process_printer::ProcessPrinterTextComponent::new()
+        .finalize(components::process_printer_text_component_static!());
+    PROCESS_PRINTER = Some(process_printer);
+
+    // Setup UART channel
     let uart_channel = nrf52_components::UartChannelComponent::new(
         uart_channel,
         mux_alarm,
@@ -275,16 +236,11 @@ pub unsafe fn main() {
         nrf52840::rtc::Rtc
     ));
 
-    // Tool for displaying information about processes.
-    let process_printer = components::process_printer::ProcessPrinterTextComponent::new()
-        .finalize(components::process_printer_text_component_static!());
-    PROCESS_PRINTER = Some(process_printer);
-
-    // Virtualize the UART channel for the console and for kernel debug.
+    // Virtualize the UART channel
     let uart_mux = components::console::UartMuxComponent::new(uart_channel, 115200)
         .finalize(components::uart_mux_component_static!());
 
-    // Create the console for user communication
+    // Create the console
     let console = components::console::ConsoleComponent::new(
         board_kernel,
         capsules_core::console::DRIVER_NUM,
@@ -292,8 +248,8 @@ pub unsafe fn main() {
     )
     .finalize(components::console_component_static!());
 
-    // Create the process console, an interactive terminal for managing processes.
-    let pconsole = components::process_console::ProcessConsoleComponent::new(
+    // Create the process console for debugging
+    let _ = components::process_console::ProcessConsoleComponent::new(
         board_kernel,
         uart_mux,
         mux_alarm,
@@ -304,12 +260,12 @@ pub unsafe fn main() {
         nrf52840::rtc::Rtc<'static>
     ));
 
-    // Create the debugger object that handles calls to `debug!()`.
+    // Setup debug writer
     components::debug_writer::DebugWriterComponent::new(uart_mux)
         .finalize(components::debug_writer_component_static!());
 
     //--------------------------------------------------------------------------
-    // USB CDC-ACM SETUP
+    // USB CDC-ACM SETUP - THE IMPORTANT PART
     //--------------------------------------------------------------------------
 
     // Create a virtual timer for USB CDC-ACM
@@ -323,9 +279,9 @@ pub unsafe fn main() {
     let strings = static_init!(
         [&str; 3],
         [
-            "Tock", // Manufacturer
+            "Tock",               // Manufacturer
             "nRF52840-DK USB CDC", // Product
-            "serial0001",    // Serial number
+            "serial0001",         // Serial number
         ]
     );
 
@@ -354,105 +310,18 @@ pub unsafe fn main() {
             nrf52::usbd::Usbd<'static>,
             VirtualMuxAlarm<'static, nrf52::rtc::Rtc<'static>>
         >>,
-        UsbSyscallDriver::new(usb_cdc, board_kernel.create_grant(capsules_extra::usb::usb_user::DRIVER_NUM, &memory_allocation_capability))
+        UsbSyscallDriver::new(
+            usb_cdc, 
+            board_kernel.create_grant(
+                capsules_extra::usb::usb_user::DRIVER_NUM, 
+                &memory_allocation_capability
+            )
+        )
     );
 
     // Enable USB CDC-ACM driver
     usb_cdc.enable();
     usb_cdc.attach();
-
-    //--------------------------------------------------------------------------
-    // BUTTONS & LEDS & GPIO SETUP
-    //--------------------------------------------------------------------------
-
-    // Configure the GPIO pins by creating the component
-    let gpio = components::gpio::GpioComponent::new(
-        board_kernel,
-        capsules_core::gpio::DRIVER_NUM,
-        components::gpio_component_helper!(
-            nrf52840::gpio::GPIOPin,
-            0 => &nrf52840_peripherals.gpio_port[Pin::P1_01],
-            1 => &nrf52840_peripherals.gpio_port[Pin::P1_02],
-            2 => &nrf52840_peripherals.gpio_port[Pin::P1_03],
-            3 => &nrf52840_peripherals.gpio_port[Pin::P1_04],
-            4 => &nrf52840_peripherals.gpio_port[Pin::P1_05],
-            5 => &nrf52840_peripherals.gpio_port[Pin::P1_06],
-            6 => &nrf52840_peripherals.gpio_port[Pin::P1_07],
-            7 => &nrf52840_peripherals.gpio_port[Pin::P1_08],
-            10 => &nrf52840_peripherals.gpio_port[Pin::P1_12],
-            11 => &nrf52840_peripherals.gpio_port[Pin::P1_13],
-            12 => &nrf52840_peripherals.gpio_port[Pin::P1_14],
-            13 => &nrf52840_peripherals.gpio_port[Pin::P1_15],
-        ),
-    )
-    .finalize(components::gpio_component_static!(nrf52840::gpio::GPIOPin));
-
-    // Configure the button pins
-    let button = components::button::ButtonComponent::new(
-        board_kernel,
-        capsules_core::button::DRIVER_NUM,
-        components::button_component_helper!(
-            nrf52840::gpio::GPIOPin,
-            (
-                &nrf52840_peripherals.gpio_port[BUTTON1_PIN],
-                kernel::hil::gpio::ActivationMode::ActiveLow,
-                kernel::hil::gpio::FloatingState::PullUp
-            ),
-            (
-                &nrf52840_peripherals.gpio_port[BUTTON2_PIN],
-                kernel::hil::gpio::ActivationMode::ActiveLow,
-                kernel::hil::gpio::FloatingState::PullUp
-            ),
-            (
-                &nrf52840_peripherals.gpio_port[BUTTON3_PIN],
-                kernel::hil::gpio::ActivationMode::ActiveLow,
-                kernel::hil::gpio::FloatingState::PullUp
-            ),
-            (
-                &nrf52840_peripherals.gpio_port[BUTTON4_PIN],
-                kernel::hil::gpio::ActivationMode::ActiveLow,
-                kernel::hil::gpio::FloatingState::PullUp
-            )
-        ),
-    )
-    .finalize(components::button_component_static!(
-        nrf52840::gpio::GPIOPin
-    ));
-
-    // Configure the LED pins
-    let led = components::led::LedsComponent::new().finalize(components::led_component_static!(
-        LedLow<'static, nrf52840::gpio::GPIOPin>,
-        LedLow::new(&nrf52840_peripherals.gpio_port[LED1_PIN]),
-        LedLow::new(&nrf52840_peripherals.gpio_port[LED2_PIN]),
-        LedLow::new(&nrf52840_peripherals.gpio_port[LED3_PIN]),
-        LedLow::new(&nrf52840_peripherals.gpio_port[LED4_PIN]),
-    ));
-
-    //--------------------------------------------------------------------------
-    // BLE RADIO SETUP
-    //--------------------------------------------------------------------------
-
-    let ble_radio = components::ble::BLEComponent::new(
-        board_kernel,
-        capsules_extra::ble_advertising_driver::DRIVER_NUM,
-        &base_peripherals.ble_radio,
-        mux_alarm,
-    )
-    .finalize(components::ble_component_static!(
-        nrf52840::rtc::Rtc,
-        nrf52840::ble_radio::Radio
-    ));
-
-    //--------------------------------------------------------------------------
-    // RANDOM NUMBER GENERATOR
-    //--------------------------------------------------------------------------
-
-    let rng = components::rng::RngComponent::new(
-        board_kernel,
-        capsules_core::rng::DRIVER_NUM,
-        &base_peripherals.trng,
-    )
-    .finalize(components::rng_component_static!(nrf52840::trng::Trng));
 
     //--------------------------------------------------------------------------
     // NRF CLOCK SETUP
@@ -467,19 +336,14 @@ pub unsafe fn main() {
     let scheduler = components::sched::round_robin::RoundRobinComponent::new(&*addr_of!(PROCESSES))
         .finalize(components::round_robin_component_static!(NUM_PROCS));
 
-    // Create the platform object
+    // Create the platform object - minimal with just USB, console, and alarm
     let platform = static_init!(
         Platform,
         Platform {
-            button,
-            ble_radio,
             console,
-            led,
-            gpio,
-            rng,
-            alarm,
             usb_cdc,
             usb_driver,
+            alarm,
             scheduler,
             systick: cortexm4::systick::SysTick::new_with_calibration(64000000),
             ipc: kernel::ipc::IPC::new(
@@ -489,9 +353,6 @@ pub unsafe fn main() {
             ),
         }
     );
-
-    // Console initialization is done in the component
-    let _ = pconsole.start();
 
     debug!("Initialization complete. Entering main loop");
 
